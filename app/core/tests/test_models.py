69d508f0bc694ffdddf290a30c6c8e0cb4476b39
Aliaksandr Litvinau <litvinaualex@gmail.com>
2022-12-07 14:32:46 +0300

    add test_new_user_email_normalized

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,10 +3,8 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-
         email = "test@gmail.com"
         password = "testpass111"
         user = get_user_model().objects.create_user(
@@ -16,3 +14,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "test@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, "testpass111")
+
+        self.assertEqual(user.email, email.lower())
