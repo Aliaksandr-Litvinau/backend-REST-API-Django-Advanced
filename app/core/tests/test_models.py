6b3b368c33e0fc0134c5eb9a3d619e0b2f78a029
Aliaksandr Litvinau <litvinaualex@gmail.com>
2022-12-07 14:49:51 +0300

    add test_new_user_invalid_email

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -21,3 +21,8 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, "testpass111")
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test user's creating with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "testpass111")
